c11fa66c8029cebfffcb145b358d55ee75085757
Asif Imtial <aimtial@gmail.com>
2023-08-11 15:29:28 +0600

    Save Oath to DB

--- migrations/20230805181250_create_table_painee.down.sql
+++
@@ -1,5 +0,0 @@
--- Add down migration script here
-ALTER TABLE oath
-DROP CONSTRAINT pk_oath_id;
-
-DROP TABLE oath;
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web::{Json, Data}, middleware::Logger};
-use chrono::{DateTime, Utc};
+use actix_web::{get, post, App, HttpResponse, HttpServer, Responder, web::{Json, Data, self}, middleware::Logger};
+use chrono::{DateTime, NaiveDate, Utc};
 use sqlx::{FromRow, Pool, Postgres, postgres::PgPoolOptions};
 use walkdir::WalkDir;
 use dotenv::dotenv;
+use log::{debug};
 use std::{error::Error, collections::HashMap};
 use html_minifier::minify;
-use serde::{Serialize, Deserialize};
-// use handlebars::Handlebars;
-// use serde_json::json;
-
+use serde::{Serialize, Deserialize, Serializer, Deserializer};
+use handlebars::Handlebars;
+use serde::ser::SerializeTuple;
 #[macro_use]
 extern crate lazy_static;
 
 lazy_static! {
     static ref FILE_CACHE: HashMap<String, String> = initialize_cache().unwrap_or_default();
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Remedy {
+    statement: String,
+    n: u32,
+    unit: String
 }
 
 fn initialize_cache() -> Result<HashMap<String, String>, Box<dyn Error>> {
@@ -73,73 +80,216 @@ async fn index() -> impl Responder {
     HttpResponse::Ok().body(content)
 }
 
+#[derive(Debug, Clone)]
+pub enum Unit {
+    Times(i32),
+    Rakah(i32),
+    Minutes(i32),
+    Hours(i32),
+    Days(i32)
+}
+impl Unit {
+    fn name(&self) -> &str {
+        match self {
+            Unit::Times(_) => "Times",
+            Unit::Rakah(_) => "Rakah",
+            Unit::Minutes(_) => "Minutes",
+            Unit::Hours(_) => "Hours",
+            Unit::Days(_) => "Days"
+        }
+    }
+}
+
+impl Serialize for Unit {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer {
+        let mut unit_tuple = serializer.serialize_tuple(2)?;
+        let _ = unit_tuple.serialize_element(&self.name());
+        let _ = match self {
+            Unit::Times(n) => unit_tuple.serialize_element(n),
+            Unit::Rakah(n) => unit_tuple.serialize_element(n),
+            Unit::Minutes(n) => unit_tuple.serialize_element(n),
+            Unit::Hours(n) => unit_tuple.serialize_element(n),
+            Unit::Days(n) => unit_tuple.serialize_element(n)
+        };
+        unit_tuple.end()
+    }
+}
+
+impl<'de> Deserialize<'de> for Unit {
+    fn deserialize<D>(deserializer: D) -> Result<Unit, D::Error>
+    where
+        D: Deserializer<'de>
+    {
+        let data: Vec<serde_json::Value> = Deserialize::deserialize(deserializer)?;
+
+        // if data.len() != 2 {
+        //     Err(serde::de::Error::custom("Expected a tuple with 2 elements"))
+        // }
+
+        let variant = data[0].as_str().ok_or_else(|| {
+            serde::de::Error::custom("First element of the tuple should be a string representing the variant")
+        })?;
+
+        let amount = data[1].as_i64().ok_or_else(|| {
+            serde::de::Error::custom("Second element of the tuple should be an integer representing the amount")
+        })? as i32;
+
+        match variant {
+            "Times" => Ok(Unit::Times(amount)),
+            "Rakah" => Ok(Unit::Rakah(amount)),
+            "Minutes" => Ok(Unit::Minutes(amount)),
+            "Hours" => Ok(Unit::Hours(amount)),
+            "Days" => Ok(Unit::Days(amount)),
+            _ => Err(serde::de::Error::custom("Unknown variant")),
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct ConditionalRemedy {
+pub struct RemedyModel {
     pub condition: String,
-    pub remedy: String
+    pub statement: String,
+    pub unit: Option<Unit>
 }
 
 #[derive(Debug, Serialize, Deserialize)]
-struct TargetData {
+struct OathModel {
     target: String,
-    remedies: Vec<ConditionalRemedy>,
+    remedies: Vec<RemedyModel>,
     penalty: String,
     until: DateTime<Utc>
 }
 
-// impl TargetData {
-//     fn to_entity(&self) -> OathEntity {
-//         OathEntity {
-//             id: None,
-//             target: self.target.clone(),
-//             remedies: types::Json(self.remedies.clone()),
-//             penalty: self.penalty.clone(),
-//             until: self.until
-//         }
-//     }
-// }
-
 #[derive(Debug, FromRow, Serialize, Deserialize)]
-pub struct CreateOathSchema {
-    pub id: Option<i32>,
+pub struct CreateUserSchema {
+    pub id: i32,
+    pub first_name: String,
+    pub last_name: Option<String>,
+    pub date_of_birth: NaiveDate,
+    pub created_at: DateTime<Utc>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RemedySchema {
+    pub id: i32,
+    pub condition: String,
+    pub statement: String,
+    pub n: Option<i32>,
+    pub unit: Option<String>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OathSchema {
+    pub id: i32,
     pub target: String,
-    pub conditional_remedies: sqlx::types::JsonValue,
     pub penalty: String,
     pub until: DateTime<Utc>
 }
 
 #[post("/save-target")]
+// async fn save_target(req: String) -> impl Responder {
 async fn save_target(
-    req_body: Json<TargetData>,
+    req_body: Json<OathModel>,
     data: Data<AppState>
 ) -> impl Responder {
-    let target_data = &req_body.0;
+    let oath_model = &req_body.0;
+
     let query_result = sqlx::query_as!(
-        CreateOathSchema,
-        "INSERT INTO oath (target, conditional_remedies, penalty, \"until\")
-        VALUES ($1, $2, $3, $4)
-        RETURNING
-            id,
-            target,
-            conditional_remedies,
-            penalty,
-            \"until\";",
-        target_data.target,
-        serde_json::json!(target_data.remedies),
-        target_data.penalty,
-        target_data.until
+        OathSchema,
+        r#"
+        INSERT INTO oath (target, penalty, until, user_id)
+        VALUES ($1, $2, $3, 1)
+        RETURNING id, target, penalty, until
+        "#,
+        oath_model.target,
+        oath_model.penalty,
+        oath_model.until
     )
     .fetch_one(&data.db)
     .await;
 
+    let oath = query_result.unwrap();
+    debug!("Oath: {:?}", &oath);
+
+    let conditions: Vec<String> =
+        oath_model.remedies
+        .iter()
+        .map(|r| r.condition.clone())
+        .collect();
+    let statements: Vec<String> =
+        oath_model.remedies
+        .iter()
+        .map(|r| r.statement.clone())
+        .collect();
+    let units: Vec<Option<&str>> =
+        oath_model.remedies
+        .iter()
+        .map(|r| r.unit.as_ref().map(|u| u.name()))
+        .collect();
+    let amounts: Vec<Option<i32>> =
+        oath_model.remedies
+        .iter()
+        .map(|r| r.unit.as_ref().map(|u| {
+            match u {
+                Unit::Times(n) => n.to_owned(),
+                Unit::Rakah(n) => n.to_owned(),
+                Unit::Minutes(n) => n.to_owned(),
+                Unit::Hours(n) => n.to_owned(),
+                Unit::Days(n) => n.to_owned(),
+            }
+        }))
+        .collect();
+
+    let query_result = sqlx::query_as!(
+        RemedySchema,
+        r#"
+        INSERT INTO remedy (condition, statement, n, unit, oath_id)
+        SELECT
+            condition,
+            statement,
+            n,
+            unit,
+            $1
+        FROM UNNEST(
+            $2::text[],
+            $3::text[],
+            $4::int[],
+            $5::text[]
+        ) AS remedy_input(
+            condition,
+            statement,
+            n,
+            unit
+        )
+        RETURNING id, condition, statement, n, unit
+        "#,
+        oath.id,
+        &conditions,
+        &statements,
+        &amounts as _,
+        &units as _
+    )
+    .fetch_all(&data.db)
+    .await;
+
     match query_result {
-        Ok(oath) => {
-            HttpResponse::Ok().body(serde_json::to_string(&oath).unwrap())
+        Ok(remedies) => {
+            debug!("Remedies: {:?}", &remedies);
+            HttpResponse::Created().body("Oath Created")
         }
         Err(e) => {
             HttpResponse::BadRequest().body(e.to_string())
         }
     }
+}
+
+#[get("/oath/{id}")]
+async fn get_all_oaths_for_user(path: web::Path<Option<i32>>) -> impl Responder {
+    let content = path.into_inner();
+    println!("{:?}", content);
+    HttpResponse::Ok().body("body")
 }
 
 #[actix_web::main]
@@ -170,6 +320,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Logger::default())
             .service(index)
             .service(save_target)
+            .service(get_all_oaths_for_user)
     })
     .bind(("127.0.0.1", 12345))?
     .run()
